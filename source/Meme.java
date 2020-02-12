c1781e90c37d630d5f25ce96e477584bfd288fc8
Carlos A. Diaz <carlosdiaz723@yahoo.com>
2020-02-11 19:22:19 -0500

    screw Java amirite guys

---
+++ source/Meme.java
@@ -0,0 +1,7 @@
+public class Meme{
+    public static void main(String[] args){
+        System.out.print("According to all known laws of aviation, there is no way that a bee should be able to fly."
+                        ,"Its wings are too small to get its fat little body off the ground."
+                        ,"The bee, of course, flies anyways. Because bees don't care what humans think is impossible.");
+    }
+}
