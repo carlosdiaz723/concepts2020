7a3fff0b86f11a0e6df2f2d4686b1f1284c21faf
Carlos Diaz <carlosdiaz723@yahoo.com>
2020-02-17 19:04:00 -0500

    basic scanner done

---
+++ source/lexicalRules.py
@@ -0,0 +1,31 @@
+
+keywords = {'remark': 'REM',
+            'newLine': '<newline>',
+            'colon': ':',
+            'pound': '#',
+            'assignment': '=',
+            'comma': ',',
+            'semicolon': ';',
+            'greaterThan': '>',
+            'lessThan': '<',
+            'multiplication': '\*',
+            'plus': '\+',
+            'minus': '-',
+            'home': 'HOME',
+            'or': 'OR',
+            'and': 'AND',
+            'goTo': 'GOTO',
+            'if': 'IF',
+            'end': 'END',
+            'print': 'PRINT',
+            'instantiation': 'LET',
+            'text': 'TEXT',
+            'openParens': '\(',
+            'closeParens': '\)'
+            }
+
+literals = {'integer': '^[+-]?(\d)*$',
+            'real': '[-+]?[0-9]*\.?[0-9]+',
+            'string': '\"(\w)*\"',
+            'printable/id': '(\w)*'
+            }
---
+++ source/tokenizer.py
@@ -0,0 +1,55 @@
+from scanner import scan
+import re
+import lexicalRules
+# BASIC VERSION
+'''
+now, tokenize
+    -scan each lexeme
+    -determine token
+'''
+
+with open('./../example.txt', 'r') as exampleFile:
+    ex1 = exampleFile.read()
+
+
+'''
+list of symbols that are seperators, just like whitespace.
+they'll all be lexemes except sometimes '.' will be inside of a float.
+'''
+symbols = ['{', '}', '(', ')', '[', ']', '.', '"', '*', '\n', ':', ',', ';',
+           '+', '-', '>', '<', '/']
+
+
+'''
+special cases where a keyword is two symbols and might be accidentally
+scanned as two different lexemes. I've tried to code out this possibility but
+we might run into problems later.
+'''
+doubleSymbols = ['..', ':=']
+
+
+# KEYWORDS houses all possible special seperators (hopefully, don't know BASIC)
+KEYWORDS = symbols + doubleSymbols
+
+
+# print it out, optional but cool to see for now
+lexemes = scan(ex1, KEYWORDS, doubleSymbols)
+
+keywordFound = False
+
+for index, lex in enumerate(lexemes):
+    keywordFound = False
+    for key in lexicalRules.keywords:
+        x = re.search(lexicalRules.keywords[key], lex)
+        if x is not None:
+            print('Lexeme {}: {}'.format(index + 1, lex).ljust(25) +
+                  'Token: {}'.format(key))
+            keywordFound = True
+            break
+    if not keywordFound:
+        for key in lexicalRules.literals:
+            y = re.search(lexicalRules.literals[key], lex)
+            if y is not None:
+                print('Lexeme {}: {}'.format(index + 1, lex).ljust(25) +
+                      'Token: {}'.format(key))
+                break
