b33642d77a5921fcdd397166a43c9dcb630c8d24
Becervan <BenCer93@gmail.com>
2020-03-20 14:44:39 -0400

    Building parser class still several issues to work through

--- source/parser.py
+++ source/parser.py
@@ -5,7 +5,7 @@ tokenList = tokenizer.getAllTokens()
 # Prints each item in the tuple object
 # Each tuple comes in the following format (validity(true/false), lexemeNumber, lexeme, tokenType(id/keyword), token)
 for token in tokenList:
-    print (tokenList.index(token), token)
+    print (token[2], token[4])
     
 '''
 
@@ -15,3 +15,24 @@ for token in tokenList:
     http://effbot.org/zone/simple-top-down-parsing.htm
     
 '''
+
+class Parser(object):
+    
+    def __init__(self, tokens):
+        # This will hold all the tokens that have been created by the scanner/lexer
+        self.tokens = tokens
+        # This will hold the token index we are parsing at
+        self.token_index = 0
+        
+    def parse(self):
+        while self.token_index < len(self.tokens):
+            
+            # Holds the type of tokens for example printable
+            token_type = self.tokens[self.token_index][0]
+            # Holds the value of tokens for example Y
+            token_value = self.tokens[self.token.index][1]
+            
+            print(token_type, token_value)
+            
+            #Increment token index by 1 so we can loop through the next totken
+            self.token.index += 1
