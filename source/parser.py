931660a6ccccba9170b2d6bde06cc36a4ad5ab84
Becervan <BenCer93@gmail.com>
2020-03-21 16:02:34 -0400

    Minor updates to parser file

--- source/parser.py
+++ source/parser.py
@@ -1,8 +1,37 @@
+'''
+Names / Emails:
+Carlos Diaz             cdiaz29@students.kennesaw.edu
+Benjamin Cervantes     bcervan4@students.kennesaw.edu
+Aydan Mufti             amufti1@students.kennesaw.edu
+
+
+College of Computing and Software Engineering
+Department of Computer Science
+---------------------------------------------
+CS4308: CONCEPTS OF PROGRAMMING LANGUAGES
+SECTION W01 – SPRING 2020
+---------------------------------------------
+Course Project
+2nd Deliverable: Parser
+
+
+File Description:
+THIS IS THE ONLY FILE THAT SHOULD BE RAN.
+It should be ran from the 'source' directory.
+This is where the list of lexemes is tokenized and printed.
+
+Note: All python files are pep8/pycodestyle compliant.
+'''
+
 import tokenizer
 
+# Will keep track of the current token
+currentToken = 0
 tokenList = tokenizer.getAllTokens()
 
-# Prints each item in the tuple object
+
+
+# Prints each tuple in the list 
 # Each tuple comes in the following format (validity(true/false), lexemeNumber, lexeme, tokenType(id/keyword), token)
 for token in tokenList:
     print (token[2], token[4])
@@ -16,7 +45,9 @@ for token in tokenList:
     
 '''
 
-class Parser(object):
+
+'''
+class parser(object):
     
     def __init__(self, tokens):
         # This will hold all the tokens that have been created by the scanner/lexer
@@ -25,14 +56,16 @@ class Parser(object):
         self.token_index = 0
         
     def parse(self):
+        
         while self.token_index < len(self.tokens):
             
             # Holds the type of tokens for example printable
-            token_type = self.tokens[self.token_index][0]
+            token_type  = self.tokens[self.token_index][0]
             # Holds the value of tokens for example Y
             token_value = self.tokens[self.token.index][1]
             
             print(token_type, token_value)
             
-            #Increment token index by 1 so we can loop through the next totken
+            #Increment token index by 1 so we can loop through the next token
             self.token.index += 1
+'''
